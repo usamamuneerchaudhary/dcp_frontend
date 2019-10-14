bbf4f9f112d36c587774f1380157008a02cc9142
usamamuneerchaudhary <info@usamamuneer.me>
2019-10-14 11:39:38 +0500

    reformat and add try catch block for remmeberCache method

--- app/Providers/TranslationServiceProvider.php
+++ app/Providers/TranslationServiceProvider.php
@@ -8,45 +8,52 @@ use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
-class TranslationServiceProvider extends ServiceProvider {
-	/**
-	 * The path to the current lang files
-	 * @var string
-	 */
-	protected $langPath;
+class TranslationServiceProvider extends ServiceProvider
+{
+    /**
+     * The path to the current lang files
+     * @var string
+     */
+    protected $langPath;
 
-	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot() {
-		if ( request()->is( 'en', 'en/*' ) ) {
-			Cache::pull( 'translations' );
-			App::setLocale( 'en' );
-			$this->langPath = resource_path( 'lang/en' );
-		} elseif ( request()->is( 'vi', 'vi/*' ) ) {
-			Cache::pull( 'translations' );
-			App::setLocale( 'vi' );
-			$this->langPath = resource_path( 'lang/vi' );
-		}
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (request()->is('en', 'en/*')) {
+            Cache::pull('translations');
+            App::setLocale('en');
+            $this->langPath = resource_path('lang/en');
+        } elseif (request()->is('vi', 'vi/*')) {
+            Cache::pull('translations');
+            App::setLocale('vi');
+            $this->langPath = resource_path('lang/vi');
+        }
 
-		Cache::rememberForever( 'translations', function () {
-			return collect( File::allFiles( $this->langPath ) )->flatMap( function ( $file ) {
-				return [
-					$translation = $file->getBasename( '.php' ) => trans( $translation ),
-				];
-			} )->toJson();
-		} );
-	}
+        try {
+            Cache::rememberForever('translations', function () {
+                return collect(File::allFiles($this->langPath))->flatMap(function ($file) {
+                    return [
+                        $translation = $file->getBasename('.php') => trans($translation),
+                    ];
+                })->toJson();
+            });
+        } catch (\Exception $e) {
+            report($e);
+        }
+    }
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register() {
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
 
-	}
+    }
 
 }
